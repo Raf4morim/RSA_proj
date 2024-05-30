fec3b014ff7d13a1585b09dccbb8fef833f375fa
Raf4morim <rafael.amorim@ua.pt>
2024-05-30 18:58:59 +0100

    icons

--- static/map.js
+++ static/map.js
@@ -4,9 +4,24 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var obuIcon = L.icon({
+var obuIconFrontCar = L.icon({
+    iconUrl: "static/front_car.png",
+    iconSize: [24, 24],
+    iconAnchor: [18, 39],
+    popupAnchor: [10, -35]
+});
+
+
+var obuIconAmbulance = L.icon({
+    iconUrl: "static/ambulance.png",
+    iconSize: [24, 24],
+    iconAnchor: [18, 39],
+    popupAnchor: [10, -35]
+});
+
+var obuIconViolatingCar = L.icon({
     iconUrl: "static/car.png",
-    iconSize: [16, 16],
+    iconSize: [24, 24],
     iconAnchor: [18, 39],
     popupAnchor: [10, -35]
 });
@@ -26,8 +41,15 @@ function obuCall() {
                 markers.forEach(delMarker);
 
                 let i = 0;
+                let obuIcon;
                 response.forEach(function(data) {
                     if (data[0] !== null && data[1] !== null) {
+                        if (data[2] == "192.168.98.30")
+                            obuIcon = obuIconFrontCar
+                        if (data[2] == "192.168.98.20")
+                            obuIcon = obuIconAmbulance
+                        if (data[2] == "192.168.98.10")
+                            obuIcon = obuIconViolatingCar
                         markers[i] = L.marker([data[0], data[1]], {icon: obuIcon}).addTo(map)
                             .bindTooltip(data[2], {permanent: false});
                         i++;
